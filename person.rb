c3471de6c449d27f40d9b1b2d1bf197d48ba2fa0
gandradep <eventsonido@gmail.com>
2023-01-03 12:08:24 -0600

    Add class Student

--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ class Person
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, name = 'Unknown', parent_permission: false)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
---
+++ student.rb
@@ -0,0 +1,12 @@
+require './person'
+# student class
+class Student < Person
+  def initialize(classroom, age)
+    super(age)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
