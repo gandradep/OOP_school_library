02b6192d6b21a1414adb1929ba1873c9cfb7740a
gandradep <eventsonido@gmail.com>
2023-01-04 13:28:55 -0600

    Fix linters errors person.rb

--- person.rb
+++ person.rb
@@ -1,10 +1,11 @@
 require './nameable'
 # definition of Person class
-class Person > Nameable
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
