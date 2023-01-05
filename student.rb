7c00c06bcd362301612f1a2b4d13d3d04797ddbd
gandradep <eventsonido@gmail.com>
2023-01-05 14:15:24 -0600

    Create relationship Classroom and Student

--- student.rb
+++ student.rb
@@ -1,12 +1,19 @@
 require './person'
 # student class
 class Student < Person
+  attr_reader :classroom
+
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
     '¯\(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
