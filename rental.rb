d68dcb0e9c6ce5dba46683d289111b16ed346d30
gandradep <eventsonido@gmail.com>
2023-01-05 15:12:34 -0600

    Complete many to many between Person and Book using Rental

--- rental.rb
+++ rental.rb
@@ -1,8 +1,15 @@
 # Rental helps with  many-to-many relationship between Person and Book
 class Rental
+  attr_reader :person, :book
   attr_accessor :date
 
-  def initialize(date)
+  def initialize(date, person, book)
     @date = date
+
+    @person = person
+    person.rentals << self
+
+    @book = book
+    book.rentals << self
   end
 end
